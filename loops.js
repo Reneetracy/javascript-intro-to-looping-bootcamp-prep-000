311c9df384a561430c058815d19a11ac4cc652da
Renee Tracy <renee.upandaway@gmail.com>
2017-09-11 10:47:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,7 +19,7 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 } do {
-  array = array.shift();
+  array = array.pop();
 } while (array.length > 0 && maybeTrue());
 return array;
 }
